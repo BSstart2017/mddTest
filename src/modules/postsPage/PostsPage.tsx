1e192c7fba910fa3ce146665e19bb26df629838a
Missio <nicepk.by@gmail.com>
2024-02-04 13:45:51 +0100

    final

--- src/modules/postsPage/PostsPage.tsx
+++ src/modules/postsPage/PostsPage.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react'
-import { ListGroup, Card, Spinner } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { ListGroup, Card, Spinner, Button } from 'react-bootstrap'
 import typicoApi from '@/api/typicodeApi'
 import { Post } from '@/modules/postsPage/components'
 import type { CommentType, PostType } from '@/types'
 
+type PostsWithCommentsType = Array<PostType & { comments: CommentType[] }>
 export const PostsPage = () => {
   const [isPending, setPending] = useState(false)
-  const [posts, setPosts] = useState<
-    Array<PostType & { comments: CommentType[] }>
-  >([])
+  const [posts, setPosts] = useState<PostsWithCommentsType>([])
+  const [showPosts, setShowPosts] = useState<PostsWithCommentsType>([])
 
   const fetchData = async () => {
     try {
@@ -30,9 +30,14 @@ export const PostsPage = () => {
         })
 
       setPosts(postData)
+      setShowPosts(postData.length >= 10 ? postData.slice(0,10) : postData)
     } catch (error) {
       console.error('Error fetching data:', error)
     }
+  }
+  
+  const addNextPosts = () => {
+    setShowPosts(prev => [...prev, ...posts.slice(prev.length, prev.length + 10)])
   }
 
   useEffect(() => {
@@ -49,10 +54,13 @@ export const PostsPage = () => {
           </Spinner>
         )}
         <ListGroup variant="flush">
-          {posts?.map((post) => (
+          {showPosts?.map((post) => (
             <Post key={post.id} post={post} comments={post.comments} />
           ))}
         </ListGroup>
+        {showPosts.length !== posts.length && <Button onClick={() => addNextPosts()}>
+         show next posts
+        </Button>}
       </Card.Body>
     </Card>
   )
